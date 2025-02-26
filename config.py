33ef0987292057f44a52959237fdde8ff821cca9
chelipika <103322024+chelipika@users.noreply.github.com>
2025-02-26 21:19:04 +0500

    ig login removed config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 TOKEN = "" # token from BotFather
-USERNAME = "" # Instagram username it | creating a new fresh account is reccomended
-PASSWORD = "" # Instagram password it
 CHANNEL_ID = -100  # замените на ID вашего канала | change ID to the desired channel id
 CHANNEL_LINK = "https://t.me/......."  # ссылка на канал | link/url to the channel | go to channel settings -> invite links -> new link -> set Admin request approval
 
