a030dbe328bf8a208273693ac951a71c358221be
chelipika <pirmatovj15@gmail.com>
2025-03-04 22:33:30 +0500

    create custom inline keyboard

--- reely/keyboards.py
+++ reely/keyboards.py
@@ -10,3 +10,10 @@ subscribe_channel = InlineKeyboardMarkup(inline_keyboard=[
 add_to_group = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Add to group", url="https://telegram.me/ReelyFastBot?startgroup=true")]
 ])
+def create_markap_kb(name, url):
+    if name == "None" or url== "None":
+        return None
+    ads_channel = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text=name, url=url)]
+    ])
+    return ads_channel
